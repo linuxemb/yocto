e2109e84be7d4297b800c9d217868eccfed1aa75
linuxemb <itss07@gmail.com>
2020-09-13 12:10:18 -0400

    test autorev

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 int main (int argc, char *argv[]) {
 	printf("Hello, Yocto \"%s\"\n", argv[0]);
 	printf("Hello,again Yocto \"%s\"\n", argv[0]);
+	printf("Hello,test AUTOREV \"%s\"\n", argv[0]);
 	return 0;
 }
 
