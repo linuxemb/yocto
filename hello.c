7e8a39b81ce5dc1035643be17bd74bcdb56430c9
linuxemb <itss07@gmail.com>
2020-09-15 15:35:50 -0400

    test update

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ int main (int argc, char *argv[]) {
 	printf("Hello,again Yocto \"%s\"\n", argv[0]);
 	printf("Hello,test AUTOREV \"%s\"\n", argv[0]);
 	printf("Hello,test PV+SRCRV \"%s\"\n", argv[0]);
+	printf("Hello,test PV+SRCRV \"%s\"\n", argv[0]);
 	return 0;
 }
 
